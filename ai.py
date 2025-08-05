e13b50eeecbd415119f876733926d06d09456c24
sduoooh <3101522606@qq.com>
2025-08-05 12:22:48 +0800

    1

--- ai.py
+++ ai.py
@@ -1,9 +1,10 @@
+import time
 from google import genai
 from google.genai import types, errors
 
 from config import GEMINI_API_KEY
 
-def get_ai_response(content):
+def _get_ai_response(content):
     client = genai.Client(api_key=GEMINI_API_KEY)
     try:
         response = client.models.generate_content(
@@ -13,10 +14,21 @@ def get_ai_response(content):
                 temperature=0.95
             )
         )
-    
+        return response.text
     except errors.APIError as e:
         raise ValueError(f"AI Error: {e.message}")
-    return response.text
+    
+def get_ai_response(content):
+    retry_attempts = 3
+    sleep_time = 2 
+    
+    while retry_attempts > 0:
+        try:
+            return _get_ai_response(content)
+        except ValueError as e:
+            time.sleep(sleep_time)
+            retry_attempts -= 1
+    raise e
 
 def get_summary(items):
     with open('prompt.txt', 'r', encoding='utf-8') as f:
