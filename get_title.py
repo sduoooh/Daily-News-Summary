92f67efdf22c67977fbcd4796e690982f6b236c5
sduoooh <3101522606@qq.com>
2025-08-22 17:52:45 +0800

    1

--- get_title.py
+++ get_title.py
@@ -1,8 +1,13 @@
 from requests import get
 from bs4 import BeautifulSoup
+from urllib.request import quote
 
-def get_page(source, time_limit="9h"):
-    url = f"https://news.google.com/search?q=allintitle%3Achina%20site%3A{source}%20when%3A{time_limit}&hl=en-US&gl=US&ceid=US%3Aen"
+def create_requests(sources):
+    return map(lambda x: quote(" OR ".join([f"site:{x[0]}/" + target for target in x[1]])).replace("/", "%2F") if len(x[1]) > 0 else f"site:{x[0]}", sources)
+
+def get_page(time_key, source):
+    source = f"{time_key}%20({source})"
+    url = f"https://news.google.com/search?q={source}&hl=en-US&gl=US&ceid=US%3Aen"
     response = get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"})
     if response.status_code != 200:
         return None
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from  get_title   import get_page, parse_html
+from  get_title   import create_requests, get_page, parse_html
 from      ai      import get_summary 
 from  send_email  import check_email, info, err
+
+import time
 
 if not check_email():
     raise Exception("Email configuration is incorrect or the SMTP server is unreachable. Please check your settings.")
 
-sources = ["www.reuters.com/world/china", "www.economist.com", "www.bloomberg.com/news/articles", "www.wsj.com", "www.scmp.com/news/china/article"]
-res = [parse_html(get_page(source)) for source in sources]
+sources = [
+    ["www.wsj.com", ["us-news", "world", "business", "politics", "economy", "tech", "finance"]],
+    ["www.reuters.com", []],
+    ["www.economist.com", []],
+    ["www.bloomberg.com", []],
+    ["www.ft.com", []]
+]
+time_str = f"after%3A{time.strftime('%Y-%m-%d', time.gmtime(time.time() - 86400))}%20before%3A{time.strftime('%Y-%m-%d', time.gmtime(time.time() + 86400))}"
+res = [parse_html(get_page(time_str, source)) for source in (create_requests(sources))]
 if None in res:
     err("Error: Failed to retrieve data from one or more sources.")
     exit(1)
@@ -14,7 +23,8 @@ res = [item for sublist in res for item in sublist]
 if len(res) == 0:
     info("No new articles found.")
     exit(0)
-res.sort(key=lambda x: (int(x['date'].split()[0]), 'minutes' in x['date'], 'hours' in x['date']))
+res.sort(key=lambda x: ('hours' in x['date'], 'minutes' in x['date'], int(x['date'].split()[0])))
+
 try:
     info(get_summary(res))
 except Exception as e:
