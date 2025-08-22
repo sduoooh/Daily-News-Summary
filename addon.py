a0a2e68d0f8877a523b9d75e16e1d2882fefb7bc
sduoooh <3101522606@qq.com>
2025-08-22 21:16:38 +0800

    fix zaobao

--- addon.py
+++ addon.py
@@ -2,6 +2,8 @@ import time
 from requests import get
 from bs4 import BeautifulSoup
 
+from config import ZAOBAO_SHADOW
+
 def date_transfer(date_string):
     if ":" in date_string:
         date_h, date_m = map(int, date_string.split(":"))
@@ -12,11 +14,12 @@ def date_transfer(date_string):
         return date_string.replace("分钟前", " minutes ago")
 
 def get_zaobao():
-    url = f"https://www.zaobao.com/realtime/china"
-    response = get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"})
+    response = get(ZAOBAO_SHADOW)
     response.encoding = 'utf-8' 
     if response.status_code != 200:
         return None
     soup = BeautifulSoup(response.text, 'html.parser')
     print(f"Fetched {len(soup.find_all('article'))} articles from Lianhe Zaobao")
     return [{"publisher": "Lianhe Zaobao", "title": article.find("a").get("title"), "date": date_transfer(article.find("span").text)} for article in soup.find_all('article')]
+
+print(get_zaobao())
