0379326df1f1407b9aa842c2e75b28d470fc56cf
sduoooh <3101522606@qq.com>
2025-09-02 14:27:54 +0800

    s

--- send_email.py
+++ send_email.py
@@ -10,7 +10,7 @@ sender = EMAIL_ADDRESS
 password = EMAIL_PASSWORD
 receivers = [RECIPIENT_EMAIL]
 smtp_server = SMTP_SERVER
-smtp_port = SMTP_PORT 
+smtp_port = int(SMTP_PORT) 
  
 def check_email():
     try:
