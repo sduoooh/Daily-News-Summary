5b5719da33e28c6916b98e3733acd8fd3f8334a8
zouxin <96587057+sduoooh@users.noreply.github.com>
2026-02-09 17:25:47 +0800

    fix src format change bug.

--- addon.py
+++ addon.py
@@ -18,11 +18,31 @@ def date_transfer(date_string):
 def check_zaobao():
     return bool(ZAOBAO_SHADOW)
 
+def raw2json(raw):
+    with open('test.txt', 'w', encoding='utf-8') as f:
+        f.write(raw)
+    l2 = BeautifulSoup(raw, 'html.parser').find_all('script')[19].text
+    start_pos = l2.index("{")
+    last_pos = l2.rindex("}") + 1
+    l2 = '[' + l2[start_pos:last_pos].replace('\\"', '"') + ']'
+    l3 = json.loads(l2)[14:-31]
+    res = []
+    res.append({"publisher": "Lianhe Zaobao", "title": l3[3], "date": l3[5]})
+    l3 = l3[17:]
+    t = []
+    for i in l3:
+        if type(i) != dict:
+            t.append(i)
+        else:
+            res.append({"publisher": "Lianhe Zaobao", "title": t[1], "date": t[2]})
+            t = []
+    res.append({"publisher": "Lianhe Zaobao", "title": t[1], "date": t[2]})
+    return res
+
 def get_zaobao():
     response = get(ZAOBAO_SHADOW)
     response.encoding = 'utf-8' 
     if response.status_code != 200:
         return None
-    soup = BeautifulSoup(response.text, 'html.parser')
-    article_json = json.loads(json.loads(soup.find_all('script')[-1].text[48:-2]))["loaderData"]['0-0']["context"]["payload"]["articles"]
-    return [{"publisher": "Lianhe Zaobao", "title": article["title"], "date": date_transfer(article["timestamp"])} for article in article_json]
+    article_json = raw2json(response.text)
+    return [{"publisher": "Lianhe Zaobao", "title": article["title"], "date": date_transfer(article["date"])} for article in article_json]
